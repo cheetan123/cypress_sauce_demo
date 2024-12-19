a17b20a545658444017c55f0f43a5d489660a528
Chetan Lashkari <chetanlashkari@Chetans-MacBook-Air.local>
2024-12-19 18:11:26 +0530

    first commit

---
+++ cypress/e2e/helpers/locators.js
@@ -0,0 +1,42 @@
+export const login = {
+    "usernameTxtBox": "input[id='user-name']",
+    "passwordTextBox": "input[id='password']",
+    "loginBtn": "input[id='login-button']",
+}
+
+export const pages = {
+    "title": ".title",
+    "productList": ".inventory_item_name ",
+    "addToCartBtn": "#add-to-cart",
+    "shoppingCartBtn": ".shopping_cart_link",
+    "removeProductBtn": "#remove",
+    "checkoutBtn": "#checkout",
+    "billingFirstnameTxtBox": "#first-name",
+    "billingLastnameTxtBox": "#last-name",
+    "billingPostalTxtBox": "#postal-code",
+    "continueBtn": "#continue",
+    "finishCheckoutBtn": "#finish",
+    "completeHeaderLbl": ".complete-header",
+    "completeTextLbl": ".complete-text",
+    "backToProductPageLnk": "#back-to-products",
+    "menu": "#react-burger-menu-btn",
+    "logoutlnk": "#logout_sidebar_link",
+    "cartItems": ".cart_item",
+    "addToCartBtns": "button[class~='btn_inventory']"
+}
+
+export const text = {
+    "billingFirstname": "Demo",
+    "billingLastname": "Project",
+    "billingPostal": "411057",
+    "checkoutOverview": "Checkout: Overview",
+    "checkoutComplete": "Checkout: Complete!",
+    "products": "Products",
+    "productTShirt": "Sauce Labs Bolt T-Shirt",
+    "cart": "Your Cart",
+    "checkoutInfo": "Checkout: Your Information",
+    "orderCompleteDetails": 'Your order has been dispatched, and will arrive just as fast as the pony can get there!',
+    "productJacket": "Sauce Labs Fleece Jacket",
+    "productBackpack": "Sauce Labs Backpack",
+    "orderCompleteGreetings": "Thank you for your order!"
+}
