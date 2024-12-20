9c4fdf4ff960ac206278fef91285ac029b6ea949
lashkarichetan <lashkari.chetan@gmail.com>
2024-12-20 15:50:37 +0530

    added web security to default browser

--- cypress.config.js
+++ cypress.config.js
@@ -12,6 +12,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://www.saucedemo.com',
     chromeWebSecurity: false,
+    webSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
